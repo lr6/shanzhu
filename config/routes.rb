83c28b3c7890b7bc1f363178e56f23ff9da740e4
zhibo <36.zhibo@gmail.com>
2022-03-18 08:45:25 +0000

    add validation_codes routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+    namespace :api do
+      namespace :v1 do
+        resources :validation_codes, only: [:create]
+      end
+    end
 end
